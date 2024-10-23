afbb90be4bd74b916135ee0a9e469046296bb9c4
Samohe89 <smorher483@iesalmudeyne.es>
2024-10-23 09:04:57 +0200

    Confirmando cambios en la rama login

--- HolaMundoV2.java
+++ HolaMundoV2.java
@@ -2,6 +2,6 @@ package Conflicto;
 
 public class HolaMundoV2 {
     public static void main (String [] args) {
-        System.out.println("Hola mundo!");
+        System.out.println("Hola mundo en la rama login");
     }
 }
